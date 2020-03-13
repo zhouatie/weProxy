da2eae6f350cdecf3d45dd4f6203e7bad12b1b85
zhouatie <1114349984@qq.com>
2020-03-14 00:38:29 +0800

    feat: 完成最基础的代理

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
-const http = require('http')
-const httpProxy = require('http-proxy')
-const fs = require('fs')
-const path = require('path')
-const proxy = httpProxy.createProxyServer({
-  ssl: {
-    key: fs.readFileSync(path.resolve(__dirname, '../config/private.pem'), 'utf8'),
-    cert: fs.readFileSync(path.resolve(__dirname, '../config/public.crt'), 'utf8')
-  },
-  target: {
-    host: '127.0.0.1',
-    port: 9000
-  }
-});
+const http = require('http');
+const net = require('net');
+const url = require('url');
 
-http.createServer({
-  key: fs.readFileSync(path.resolve(__dirname, '../config/private.pem'), 'utf8'),
-  cert: fs.readFileSync(path.resolve(__dirname, '../config/public.crt'), 'utf8')
-}, async (res, req) => {
-  console.log(req, 'reqqq')
-  proxy.web(res, req);
-}).listen(8000)
+function request(cReq, cRes) {
+  const u = url.parse(cReq.url);
 
-proxy.on('error', (err, req, res) => {
-  res.writeHead(500, {
-    'Content-Type': 'text/plain'
+  const options = {
+    hostname: u.hostname,
+    port: u.port || 80,
+    path: u.path,
+    method: cReq.method,
+    headers: cReq.headers
+  };
+  console.log(options, 'options')
+  const pReq = http.request(options, function (pRes) {
+    cRes.writeHead(pRes.statusCode, pRes.headers);
+    pRes.pipe(cRes);
+  }).on('error', function (e) {
+    console.log(e, 'eee')
+    cRes.end();
   });
 
-  res.end('Something went wrong. And we are reporting a custom error message.');
-});
+  cReq.pipe(pReq);
+}
+
+function connect(cReq, cSock) {
+  const u = url.parse('http://' + cReq.url);
+
+  const pSock = net.connect(u.port, u.hostname, function () {
+    cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+    pSock.pipe(cSock);
+  }).on('error', function (e) {
+    console.log(e, 'eeeeee')
+    cSock.end();
+  });
+
+  cSock.pipe(pSock);
+}
+
+http.createServer()
+  .on('request', request)
+  .on('connect', connect)
+  .listen(8000, '0.0.0.0');
