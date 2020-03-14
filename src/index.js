378db244fb042d1dc4eb32268d5fe79063f721f9
zhouatie <1114349984@qq.com>
2020-03-14 23:28:39 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,44 +1,49 @@
-const http = require('http');
-const net = require('net');
-const url = require('url');
-
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var net = require('net');
+var url = require('url');
+var path = require('path')
 function request(cReq, cRes) {
-  const u = url.parse(cReq.url);
+    var u = url.parse(cReq.url);
+    console.log(u, 'u')
+    var options = {
+        hostname : u.hostname, 
+        port     : u.port || 80,
+        path     : u.path,       
+        method     : cReq.method,
+        headers     : cReq.headers
+    };
 
-  const options = {
-    hostname: u.hostname,
-    port: u.port || 80,
-    path: u.path,
-    method: cReq.method,
-    headers: cReq.headers
-  };
-  console.log(options, 'options')
-  const pReq = http.request(options, function (pRes) {
-    cRes.writeHead(pRes.statusCode, pRes.headers);
-    pRes.pipe(cRes);
-  }).on('error', function (e) {
-    console.log(e, 'eee')
-    cRes.end();
-  });
+    var pReq = http.request(options, function(pRes) {
+        cRes.writeHead(pRes.statusCode, pRes.headers);
+        pRes.pipe(cRes);
+    }).on('error', function(e) {
+        cRes.end();
+    });
 
-  cReq.pipe(pReq);
+    cReq.pipe(pReq);
 }
 
 function connect(cReq, cSock) {
-  const u = url.parse('http://' + cReq.url);
+    var u = url.parse('http://' + cReq.url);
 
-  const pSock = net.connect(u.port, u.hostname, function () {
-    cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
-    pSock.pipe(cSock);
-  }).on('error', function (e) {
-    console.log(e, 'eeeeee')
-    cSock.end();
-  });
+    var pSock = net.connect(u.port, u.hostname, function() {
+        cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+        pSock.pipe(cSock);
+    }).on('error', function(e) {
+        cSock.end();
+    });
 
-  cSock.pipe(pSock);
+    cSock.pipe(pSock);
 }
 
-http.createServer()
-  .on('request', request)
-  .on('connect', connect)
-  .listen(8000, '0.0.0.0');
+var options = {
+    key: fs.readFileSync(path.resolve(__dirname, '../config/private.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, '../config/public.crt'))
+};
+
+https.createServer(options)
+    .on('request', request)
+    .on('connect', connect)
+    .listen(8000, '0.0.0.0');
--- src/test.js
+++ src/test.js
@@ -1,10 +1,30 @@
-const Koa = require('koa')
+"use strict";
 
-const app = new Koa()
+var koa = require('koa');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var enforceHttps = require('koa-sslify').default;
+var path = require('path')
+var app = new koa();
 
-app.use((context) => {
-  console.log(context)
-  context.body = 'haha'
-})
+// Force HTTPS on all page
+app.use(enforceHttps());
 
-app.listen(9000)
+// index page
+app.use(function * (next) {
+    this.body = "hello world from " + this.request.url;
+});
+
+// SSL options
+var options = {
+  key: fs.readFileSync(path.resolve(__dirname, '../config/private.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, '../config/public.crt'))
+};
+
+// start the server
+http.createServer(app.callback()).listen(80);
+https.createServer(options, app.callback()).listen(443);
+
+//
+console.log('https server is running');
